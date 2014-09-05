39a14077497905b1d4250eb5ae84b43a5bd10040
Angelboy <scwuaptx@gmail.com>
2014-09-05 21:19:50 +0800

    modify fonts problem

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ vim +PluginInstall +qall
 sudo apt-get install python-fontforge
 wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
 ~/.vim/bundle/vim-powerline/fontpatcher/fontpatcher Ubuntu\ Mono\ derivative\ Powerline.ttf
+cd
+mkdir ~/.fonts
 cp Ubuntu\ Mono\ derivative\ Powerline-Powerline.ttf ~/.fonts
 sudo fc-cache -vf
-vim +PowerlineClearCache
+vim +PowerlineClearCache +qall
 
