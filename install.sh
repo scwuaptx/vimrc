b6aafc4c09eaaffcc5057d37a85ef0714e5d2618
Angelboy <scwuaptx@gmail.com>
2014-09-05 20:33:05 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get install git -y
 sudo apt-get install curl -y
 apt-get install build-essential cmake python-dev -y
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/scwuaptx/vimrc
 cd vimrc
 cp .vimrc ~/.vimrc
