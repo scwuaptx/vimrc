bf41adf3fc779312a280aaa225b893b3f4750b7d
mcl <mcl@mcl-ThinkPad-X230.(none)>
2014-09-06 20:52:01 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20
 ~/.vim/bundle/vim-powerline/fontpatcher/fontpatcher Ubuntu\ Mono\ derivative\ Powerline.ttf
 cd
 mkdir ~/.fonts
-cp Ubuntu\ Mono\ derivative\ Powerline-Powerline.ttf ~/.fonts
+cp *Powerline-Powerline.ttf ~/.fonts/
 sudo fc-cache -vf
 vim +PowerlineClearCache +qall
 
