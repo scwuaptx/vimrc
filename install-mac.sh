b998407dfc9d9ee35882da24ff70caf9bd04b8ff
angelboy <scwuaptx@gmail.com>
2014-11-10 21:21:32 +0800

    modify mac version

--- install-mac.sh
+++ install-mac.sh
@@ -6,6 +6,6 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/scwuaptx/vimrc
 
 cd vimrc
-cp .vimrc ~/.vimrc
+cp .vimrc-mac ~/.vimrc
 vim +PluginInstall +qall
 cp -rf vimrc/snippets ~/.vim/
